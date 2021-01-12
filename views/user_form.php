9c7eb55f13757e2095119709574e1cfe87a921ad
Timothy Jounier <timothyjounier@gmail.com>
2021-01-12 14:10:56 +0100

    Rajout is-invalid

--- views/user_form.php
+++ views/user_form.php
@@ -2,29 +2,29 @@
     <!-- nom -->
     <div class="mb-3">
         <label for="name" class="form-label">Nom</label>
-        <input type="text" class="form-control" id="name" name="name" pattern="[A-Za-z-éèêëàâäôöûüç' ]+" title="Lettres majuscule et minuscule uniquement" required value="<?= $name ?? ''?>">
-        <div id="name_error" class="form-text"><?= $errorsArray['name_error'] ?? ''?></div>
+        <input type="text" class="form-control <?= isset($errorsArray['name_error']) ? 'is-invalid' : ''?>" id="name" name="name" pattern="[A-Za-z-éèêëàâäôöûüç' ]+" title="Lettres majuscule et minuscule uniquement" required value="<?= $name ?? ''?>">
+        <div id="name_error" class="text-danger"><?= $errorsArray['name_error'] ?? ''?></div>
     </div>
   
     <!-- prénom -->
     <div class="mb-3">
         <label for="firstname" class="form-label">Prénom</label>
-        <input type="text" class="form-control" id="firstname" name="firstname" pattern="[A-Za-z-éèêëàâäôöûüç' ]+" title="Lettres majuscules et minuscules uniquement" required value="<?= $firstname ?? ''?>">
-        <div id="firstname_error" class="form-text"><?= $errorsArray['firstname_error'] ?? ''?></div>
+        <input type="text" class="form-control <?= isset($errorsArray['firstname_error']) ? 'is-invalid' : ''?>" id="firstname" name="firstname" pattern="[A-Za-z-éèêëàâäôöûüç' ]+" title="Lettres majuscules et minuscules uniquement" required value="<?= $firstname ?? ''?>">
+        <div id="firstname_error" class="text-danger"><?= $errorsArray['firstname_error'] ?? ''?></div>
     </div>
 
     <!-- Mail -->
     <div class="mb-3">
         <label for="mail" class="form-label">Mail</label>
-        <input type="email" class="form-control" id="mail" name="mail" title="Votre adresse mail n'est pas valide" required value="<?= $mail ?? ''?>">
-        <div id="mail_error" class="form-text"><?= $errorsArray['mail_error'] ?? ''?></div>
+        <input type="email" class="form-control <?= isset($errorsArray['mail_error']) ? 'is-invalid' : ''?>" id="mail" name="mail" title="Votre adresse mail n'est pas valide" required value="<?= $mail ?? ''?>">
+        <div id="mail_error" class="text-danger"><?= $errorsArray['mail_error'] ?? ''?></div>
     </div>
 
     <!-- date de naissance -->
     <div class="mb-3">
         <label for="birthDate" class="form-label">Date de naissance</label>
         <input type="date" class="form-control" id="birthDate" name="birthDate" required value="<?= $birthDate ?? ''?>">
-        <div id="birthDate_error" class="form-text"><?= $errorsArray['birthDate_error'] ?? ''?></div>
+        <div id="birthDate_error" class="text-danger"><?= $errorsArray['birthDate_error'] ?? ''?></div>
     </div>
 
     <!-- pays de naissance -->
@@ -32,41 +32,41 @@
         <label for="birthCountry" class="form-label">Pays de naissance</label>
         <select class="form-select" id="birthCountry" name="birthCountry" required >
             <option>Choisissez votre pays de naissance</option>
-            <option value="France" <?= ($birthCountry == 'France') ? 'selected' : '' ?> >France</option>
-            <option value="Italie" <?= ($birthCountry == 'Italie') ? 'selected' : '' ?> >Italie</option>
+            <option value="France" <?= (isset($birthCountry) && $birthCountry == 'France') ? 'selected' : '' ?> >France</option>
+            <option value="Italie" <?= (isset($birthCountry) && $birthCountry == 'Italie') ? 'selected' : '' ?> >Italie</option>
             <option value="Grande Bretagne" <?= ($birthCountry == 'Grande Bretagne') ? 'selected' : '' ?> >Grande Bretagne</option>
             <option value="Mars">Mars</option>
         </select>     
-        <div id="birthCountry_error" class="form-text"><?= $errorsArray['birthCountry_error'] ?? ''?></div>
+        <div id="birthCountry_error" class="text-danger"><?= $errorsArray['birthCountry_error'] ?? ''?></div>
     </div>
 
     <!-- Téléphone -->
     <div class="mb-3">
         <label for="phone" class="form-label">Téléphone</label>
-        <input type="tel" class="form-control" id="phone" name="phone" pattern="(01|02|03|04|05|06|07|08|09)[ \.\-]?[0-9]{2}[ \.\-]?[0-9]{2}[ \.\-]?[0-9]{2}[ \.\-]?[0-9]{2}" title="Votre numéro de téléphone n'est pas valide format requis : 0000000000" value="<?= $phone ?? ''?>">
-        <div id="phone_error" class="form-text"><?= $errorsArray['phone_error'] ?? ''?></div>
+        <input type="tel" class="form-control <?= isset($errorsArray['phone_error']) ? 'is-invalid' : ''?>" id="phone" name="phone" pattern="(01|02|03|04|05|06|07|08|09)[ \.\-]?[0-9]{2}[ \.\-]?[0-9]{2}[ \.\-]?[0-9]{2}[ \.\-]?[0-9]{2}" title="Votre numéro de téléphone n'est pas valide format requis : 0000000000" value="<?= $phone ?? ''?>">
+        <div id="phone_error" class="text-danger"><?= $errorsArray['phone_error'] ?? ''?></div>
     </div>
 
 
     <!-- URL -->
     <div class="mb-3">
         <label for="link" class="form-label">Lien Codecademy</label>
-        <input type="text" class="form-control" id="link" name="link" pattern="/^(?:http(s)?://)?[\w.-]+(?:.[\w.-]+)+[\w-._~:/?#[]@!$&'()*+,;=.]+$/" title="Votre URL n'est pas valide" value="<?= $link ?? ''?>">
-        <div id="link_error" class="form-text"><?= $errorsArray['link_error'] ?? ''?></div>
+        <input type="text" class="form-control <?= isset($errorsArray['link_error']) ? 'is-invalid' : ''?>" id="link" name="link" pattern="/^(?:http(s)?://)?[\w.-]+(?:.[\w.-]+)+[\w-._~:/?#[]@!$&'()*+,;=.]+$/" title="Votre URL n'est pas valide" value="<?= $link ?? ''?>">
+        <div id="link_error" class="text-danger"><?= $errorsArray['link_error'] ?? ''?></div>
     </div>
 
     <!-- Numéro Pole Emploi -->
     <div class="mb-3">
         <label for="job_number" class="form-label">Numéro pole emploi</label>
-        <input type="text" class="form-control" id="job_number" name="job_number" pattern="[0-9]{7}[A-Z]{1}" title="Votre numéro pôle emploi n'est pas valide" value="<?= $job_number ?? ''?>">
-        <div id="job_number_error" class="form-text"><?= $errorsArray['job_number_error'] ?? ''?></div>
+        <input type="text" class="form-control <?= isset($errorsArray['job_number_error']) ? 'is-invalid' : ''?>" id="job_number" name="job_number" pattern="[0-9]{7}[A-Z]{1}" title="Votre numéro pôle emploi n'est pas valide" value="<?= $job_number ?? ''?>">
+        <div id="job_number_error" class="text-danger"><?= $errorsArray['job_number_error'] ?? ''?></div>
     </div>
 
     <!-- Badge Number -->
     <div class="mb-3">
         <label for="badge_number" class="form-label">Nombre de badge</label>
-        <input type="number" class="form-control" id="badge_number" name="badge_number" pattern="[0-9]{7}[A-Z]{1}" title="Votre numéro de badge n'est pas valide" value="<?= $badge_number ?? ''?>" min="0" max="200">
-        <div id="badge_number_error" class="form-text"><?= $errorsArray['badge_number_error'] ?? ''?></div>
+        <input type="number" class="form-control <?= isset($errorsArray['badge_number_error']) ? 'is-invalid' : ''?>" id="badge_number" name="badge_number" pattern="[0-9]{7}[A-Z]{1}" title="Votre numéro de badge n'est pas valide" value="<?= $badge_number ?? ''?>" min="0" max="200">
+        <div id="badge_number_error" class="text-danger"><?= $errorsArray['badge_number_error'] ?? ''?></div>
     </div>
 
 <!-- diplome -->
